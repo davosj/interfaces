a01578f24d68c9f04241cd6889cd47ba7c29fee1
davosj <49295580+davosj@users.noreply.github.com>
2019-04-08 16:29:50 -0500

    Update template.js

--- template.js
+++ template.js
@@ -23,8 +23,15 @@ pages = [[
     "isLocal": true,
     "ontouchstart": "if(this.value == this.max) { control.showToolbar(); } else { control.hideToolbar(); }",
     "label": "menu",
+    
+  
 },
-
+ {
+     "name" : "myButton",
+     "type" : "Button",
+     "x" : 0, "y" : 0,
+     "width" : .25, "height" : .25,
+}
     
 ]
 
