9bda38c579fab8fb8d0096b45e8b06156f288a6f
davosj <49295580+davosj@users.noreply.github.com>
2019-04-08 19:08:18 -0500

    Update template.js

--- template.js
+++ template.js
@@ -7,7 +7,7 @@ pages = [[
     {
     "name": "Referencia",
     "type": "Label",
-    "x": .16, "y": .895,
+    "x": .16, "y": .95,
     "width": .8, "height": .8,
     "value": "Interfaz diseñada por Jorge Davo Sainz, con recursos de Charles Roberts",
     "backgroundColor": "rgba(0,0,0,1)",
